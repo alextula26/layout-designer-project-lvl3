23ac265e1d5206f389179980790fc91681cef94d
alextula26 <alex_marc86@mail.ru>
2021-04-01 10:48:39 +0300

    added classes for padding in main layout pug

--- gulp/styles.js
+++ gulp/styles.js
@@ -2,7 +2,7 @@ const gulp = require('gulp');
 const scss = require('gulp-sass');
 
 module.exports = function styles() {
-    return gulp.src('./app/scss/index.scss')
+    return gulp.src('./app/scss/custom.scss')
         .pipe(scss())
         .pipe(gulp.dest('./dist/css/'))
 }
