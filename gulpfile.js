c3dde054e743e81dfc00cd2c66b9278529586feb
alextula26 <alex_marc86@mail.ru>
2021-04-01 17:20:34 +0300

    contacts component

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 const gulp = require('gulp');
 const pug2html = require('./gulp/pug');
 const styles = require('./gulp/styles');
+const images = require('./gulp/images');
+
 
 const { series } = gulp;
 
-exports.default = series(pug2html, styles);
+exports.default = series(pug2html, styles, images);
